5703d23e76faecb82d1566d6c9889b634b0506c9
YURI08 <147913818+Yuri08loveElaina@users.noreply.github.com>
2025-06-18 09:53:53 +0700

    Update kernel.c

--- elainaos-v0.1/Kernel/kernel.c
+++ elainaos-v0.1/Kernel/kernel.c
@@ -1,9 +1,14 @@
-void main() {
-    char *video = (char*)0xb8000;
-    const char *msg = "ElainaOS v0.1 Ready\n";
-    for (int i = 0; msg[i]; i++) {
-        video[i * 2] = msg[i];
+void print(const char* str) {
+    char* video = (char*)0xB8000;
+    int i = 0;
+    while (str[i]) {
+        video[i * 2] = str[i];
         video[i * 2 + 1] = 0x07;
+        i++;
     }
-    while (1) asm volatile("hlt");
+}
+
+void kernel_main() {
+    print("Welcome to ElainaOS v0.1");
+    while (1);
 }
