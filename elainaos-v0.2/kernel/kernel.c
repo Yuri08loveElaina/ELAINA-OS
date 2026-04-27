742c6543cff36b0d03c6149f19be81f71527fdec
YURI08 <147913818+Yuri08loveElaina@users.noreply.github.com>
2026-04-27 12:41:15 +0700

    Enhance error messages and update welcome text

--- elainaos-v0.2/kernel/kernel.c
+++ elainaos-v0.2/kernel/kernel.c
@@ -2,33 +2,35 @@
 #include "idt.h"
 #include "pic.h"
 #include "keyboard.h"
-#include "port_io.h"
+#include "port_io.h" 
 
 void divide_by_zero_handler(struct registers *r) {
-    vga_print("\nFATAL: Divide by Zero\n", VGA_RED);
+    vga_print("\nFATAL ERROR: Divide by Zero!\n", VGA_RED);
     asm volatile("cli; hlt"); 
 }
 
 void gpf_handler(struct registers *r) {
-    vga_print("\nFATAL: GPF\n", VGA_RED);
+    vga_print("\nFATAL ERROR: General Protection Fault!\n", VGA_RED);
     asm volatile("cli; hlt");
 }
 
 void kernel_main() {
     vga_init();
-    pic_remap(); 
+    pic_remap();      //  đang mask hết cứng ngắc
     idt_install();
+    
     register_isr_handler(0, divide_by_zero_handler);
     register_isr_handler(13, gpf_handler);
     
     keyboard_install();
 
-    // 0xFD = 1111 1101 (mở bit 1)
-    outb(PIC1_DATA, 0xFD);
+    // bật lại IRQ1 . PIC mask hết mà không unmask lại thì gõ búa cũng không có tín hiệu
+    outb(PIC1_DATA, inb(PIC1_DATA) & ~0x02);
 
     asm volatile("sti");
 
-    vga_print("ElainaOS v0.1\n", VGA_LIGHT_GREEN);
+    vga_print("Welcome to ElainaOS v0.2 (32-bit Protected Mode)\n", VGA_LIGHT_GREEN);
+    vga_print("VGA Driver, IDT, PIC remapped & Keyboard loaded.\n", VGA_LIGHT_GREY);
     vga_print("> ", VGA_YELLOW);
 
     while(1) {
