d264020c6b6814981f3c37a9fc2baf9296a1a49e
YURI08 <147913818+Yuri08loveElaina@users.noreply.github.com>
2026-04-27 12:30:21 +0700

    Create pic.c

---
+++ elainaos-v0.2/kernel/pic.c
@@ -0,0 +1,38 @@
+#include "pic.h"
+
+void pic_remap(void) {
+    // ICW1
+    outb(PIC1_COMMAND, 0x11);
+    io_wait();
+    outb(PIC2_COMMAND, 0x11);
+    io_wait();
+
+    // ICW2: remap offset
+    outb(PIC1_DATA, 0x20);
+    io_wait();
+    outb(PIC2_DATA, 0x28);
+    io_wait();
+
+    // ICW3: cascade
+    outb(PIC1_DATA, 0x04);
+    io_wait();
+    outb(PIC2_DATA, 0x02);
+    io_wait();
+
+    // ICW4: 8086 mode
+    outb(PIC1_DATA, 0x01);
+    io_wait();
+    outb(PIC2_DATA, 0x01);
+    io_wait();
+
+    // mask hết lại (0xFF)
+    outb(PIC1_DATA, 0xFF);
+    outb(PIC2_DATA, 0xFF);
+}
+
+void pic_send_eoi(uint8_t irq) {
+    if (irq >= 8) {
+        outb(PIC2_COMMAND, PIC_EOI);
+    }
+    outb(PIC1_COMMAND, PIC_EOI);
+}
